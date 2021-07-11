b30bdf96fba62f95f813092cbd99a081548a54f2
JakeRose689 <jake.tenorman.2015@gmail.com>
2021-07-11 19:49:59 -0400

    Completed 3/5 of the objectives of the project. Pushing changes for backup.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,7 +5,14 @@ file_path=("Resources/election_data.csv")
 
 total_votes=0
 candidates=[]
-
+khan=0
+correy=0
+li=0
+otooley=0
+khan_percent=0
+correy_percent=0
+li_percent=0
+otooley_percent=0
 
 with open(file_path) as data:
     reader=csv.reader(data)
@@ -17,6 +24,26 @@ with open(file_path) as data:
         if row[2] not in candidates:
             candidates.append(row[2])
 
+        if row[2]=='Khan':
+            khan+=1
+
+        if row[2]=='Correy':
+            correy+=1
+
+        if row[2]=='Li':
+            li+=1
+
+        if row[2]=="O'Tooley":
+            otooley+=1
+        
+    khan_percent=khan/total_votes
+    correy_percent=correy/total_votes
+    li_percent=li/total_votes
+    otooley_percent=otooley/total_votes
 
     print(total_votes)
     print(candidates)
+    print(khan)
+    print(correy)
+    print(li)
+    print(otooley)
